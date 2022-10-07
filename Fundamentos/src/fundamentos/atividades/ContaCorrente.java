7c05ee7ce4d81da236592a35ab79f2cfc1aa4ad1
Diego <luizdiego.silvabarbosa@gmail.com>
2022-10-06 22:35:36 -0300

    Add classe Main e atividade exception

--- Fundamentos/src/fundamentos/atividades/ContaCorrente.java
+++ Fundamentos/src/fundamentos/atividades/ContaCorrente.java
@@ -10,20 +10,6 @@ public class ContaCorrente
     private Double saldoDisponivel;
     private Boolean contaCancelada;
 
-    public static void main(String [] arguments)
-    {
-        ContaCorrente cliente1 = new ContaCorrente("Diego", 123, 456);
-        ContaCorrente cliente2 = new ContaCorrente("Joao", 321, 654);
-        LocalDate dataInicial = LocalDate.now();
-        LocalDate dataFinal = dataInicial.plusMonths(1);
-
-        cliente1.depositar(1000.0);
-        cliente1.sacar(500.0);
-        cliente1.transferir(500.0, cliente2);
-        cliente1.consultarExtrato(dataInicial, dataFinal);
-        cliente1.cancelarConta("To sem dinheiro :(");
-    }
-
     public ContaCorrente(String nomeCliente, Integer numeroConta, Integer numeroAgencia)
     {
         this.nomeCliente = nomeCliente;
