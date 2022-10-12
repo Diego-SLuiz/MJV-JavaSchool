dc66cbe84c698598b7a75599f0c5b1c037fb8ee5
Diego <luizdiego.silvabarbosa@gmail.com>
2022-10-12 00:42:05 -0300

    Renomeando classe Extrato para Operacao

--- controle-financeiro-app/src/main/java/conta/Operacao.java
+++ controle-financeiro-app/src/main/java/conta/Operacao.java
@@ -2,26 +2,18 @@ package conta;
 
 import java.time.LocalDate;
 
-public class Extrato
+public class Operacao
 {
     LocalDate data;
-    String transacao;
+    TipoOperacao tipo;
+    String descricao;
     Double valor;
 
-    public Extrato(LocalDate data, String transacao, Double valor)
+    public Operacao(LocalDate data, TipoOperacao tipo, String descricao, Double valor)
     {
         this.data = data;
-        this.transacao = transacao;
+        this.tipo = tipo;
+        this.descricao = descricao;
         this.valor = valor;
-    }
-
-    @Override
-    public String toString()
-    {
-        StringBuilder representacao = new StringBuilder();
-        representacao.append("Data: " + this.data.toString() + "\n");
-        representacao.append("Transacao: " + this.transacao + "\n");
-        representacao.append("Valor: " + this.valor + "\n");
-        return representacao.toString();
     }
 }
